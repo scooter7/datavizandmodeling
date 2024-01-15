6e7fda9661955eaeee885649bb06b7996b46d707
scooter7 <scooter.vineburgh@gmail.com>
2024-01-15 10:45:50 -0600

    Update datavis.py

--- datavis.py
+++ datavis.py
@@ -64,12 +64,16 @@ def create_pivot_table(data, index_column, values_column):
     data[index_column] = data[index_column].astype(str)
     data[values_column] = data[values_column].astype(str)
 
-    pivot_table = pd.pivot_table(data, index=index_column, values=values_column, aggfunc='count', fill_value=0)
-    pivot_table.columns.name = None  # Remove the columns' name
-    pivot_table.reset_index(inplace=True)
+    pivot_table = pd.pivot_table(data, index=index_column, columns=values_column, values=values_column, aggfunc='count', fill_value=0)
+    
+    # Flatten the pivot table and reset the index
+    flat_pivot_table = pivot_table.stack(level=0).reset_index()
+
+    # Rename the columns for better clarity
+    flat_pivot_table.columns = [' '.join(map(str, col_tuple)) for col_tuple in flat_pivot_table.columns.values]
 
     # Display using st.dataframe
-    st.dataframe(pivot_table)
+    st.dataframe(flat_pivot_table)
 
 def detect_mixed_type_columns(df):
     mixed_type_columns = {}
